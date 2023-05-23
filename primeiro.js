31a6db71fab582f5311e8a74544a20bffe8f27a5
Phelipe Afonso <74362146+ph-afonso@users.noreply.github.com>
2023-05-23 14:14:32 -0300

    first commit

---
+++ primeiro.js
@@ -0,0 +1,3 @@
+// Imprimindo duas mensagens no terminal
+console.log('Hellp, World!')
+console.log('Bem-vindo ao curso de JavaScript!')
---
+++ variaveis.js
@@ -0,0 +1,29 @@
+//Variáveis
+
+let idade = 23; //Criando a primeira variável
+idade = 24; //Alterando o valor de uma variável
+console.log(idade);
+
+// Tipos de variáveis
+
+let numero = 30; //Number
+let altura = 1.77; //Number
+let nome = 'Phelipe Afonso'; //String(Text)
+let estudando = true; //Booleano (true or false);
+
+console.log(numero, typeof numero);
+console.log(nome, typeof nome);
+console.log(altura, typeof altura);
+console.log(estudando, typeof estudando);
+
+let outra; 
+console.log(outra);
+
+let nula = null;
+console.log(nula);
+
+var variavel = 30; //Não utilizar, boa prática é usar let
+console.log(variavel);
+
+const constante = 100;
+console.log(constante);
