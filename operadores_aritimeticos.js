89d9746f4ac801856046bf6614d7bfdfed1fad26
Phelipe Afonso <phelipeafonso.freitas@gmail.com>
2023-05-23 23:01:41 -0300

    Operadores

---
+++ operadores_aritimeticos.js
@@ -0,0 +1,18 @@
+let primeiroNumero = 20;
+let segundoNumero = 2;
+
+console.log(`Soma = ${primeiroNumero + segundoNumero}`);
+console.log(`Subtração = ${primeiroNumero - segundoNumero}`);
+console.log(`Multiplicação = ${primeiroNumero * segundoNumero}`);
+console.log(`Divisão = ${primeiroNumero / segundoNumero}`);
+console.log(`Exponenciação = ${primeiroNumero ** segundoNumero}`);
+console.log(`Resto da divisão = ${primeiroNumero % segundoNumero}`);
+
+//Precedência de operadores
+
+console.log(3 + 7 * 3);
+console.log((3 + 7) * 3);
+
+//Biblioteca Math
+
+console.log('A raiz de 16 é: ' + Math.sqrt(16));
